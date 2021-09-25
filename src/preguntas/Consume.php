03898913a894ea9851d870792ac3d81bcd13015f
OsvaldoGDelRio <osdelrio_0@hotmail.com>
2021-09-25 00:43:21 -0700

    Removiendo declaraciones de dependencia no usada

--- src/preguntas/Consume.php
+++ src/preguntas/Consume.php
@@ -11,7 +11,7 @@ class Consume
 
     /**
      *
-     * @param PreguntaInterface $preguntaUno
+     * @param PreguntaUno $preguntaUno
      */
     public function __construct(PreguntaUno $preguntaUno)
     {
